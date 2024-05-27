c396931186dcdc6cf75e94e66e62c84d589fd359
Jean-Michel Dusserre <jedusser@z4r1p1.42lyon.fr>
2024-05-27 16:12:10 +0200

    dd

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jedusser <jedusser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 13:16:15 by jedusser          #+#    #+#             */
-/*   Updated: 2024/05/27 15:00:54 by jedusser         ###   ########.fr       */
+/*   Updated: 2024/05/27 16:00:30 by jedusser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <dirent.h>
+#include <fcntl.h>
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -62,8 +63,7 @@ int	exec_found(const char *dirname, char *exec_searched)
 	}
 	closedir(dir);
 	return (0);
-}
-
+} 
 // this function checks if exec is found in all concerned directories 
 // and returns the directory where it has been found.
 
@@ -110,9 +110,12 @@ int main(int argc, char **argv, char **envp)
 	char	*cmd1_path;
 	char	*cmd2_path;
 	char	*directory;
-	int filedes[2];
+	//int filedes[2];
 	pid_t	pid;
-	
+	int file = open ("cmd_result.txt", O_WRONLY | O_CREAT, 0777);
+	int file2 = dup2(file, STDOUT_FILENO);
+	//close(file);
+	//printf("hello");
 	directory = check_all_dirs(envp, argv[1]);
 	if (!directory)
 		return 1;
@@ -133,18 +136,18 @@ int main(int argc, char **argv, char **envp)
 	if(pid > 0)
 		waitpid(pid, NULL, 0);
 	
-	if(!argv[2])
-		return (1);
-	else if(argv[2][0] != '-')
-	{
-		directory = check_all_dirs(envp, argv[2]);
-		cmd2_path = ft_concat_path(&directory, argv[2]);
-	}
-	else
-	{
-		directory = check_all_dirs(envp, argv[3]);
-		cmd2_path = ft_concat_path(&directory, argv[3]);
-	}
+	// if(!argv[2])
+	// 	return (1);
+	// else if(argv[2][0] != '-')
+	// {
+	// 	directory = check_all_dirs(envp, argv[2]);
+	// 	cmd2_path = ft_concat_path(&directory, argv[2]);
+	// }
+	// else
+	// {
+	// 	directory = check_all_dirs(envp, argv[3]);
+	// 	cmd2_path = ft_concat_path(&directory, argv[3]);
+	// }
 
 	return 0;
 }
